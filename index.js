9b717396af6a4d754511d413820d12cf72e1566e
Chinta Sunil Varma <160120737114.sunilvarma@gmail.com>
2023-04-08 10:23:08 +0530

    fianl

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ function fet()
 {
     
     request('https://jsonplaceholder.typicode.com/todos', function (error, response, body) {
-      
+      return body;
       console.log('body:', body); // Print the HTML for the Google homepage.
     });
 }
+app.get('/api/data',(req,res)=>
+{
+    request('https://jsonplaceholder.typicode.com/todos', function (error, response, body) {
+        res.send(body)
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+    
+    
+})
 
 app.listen('5000', () => {
     console.log('server succesfuly listening');
-    fet()
+   
    
    
 })
